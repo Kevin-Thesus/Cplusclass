bc8ad8a10e153bd2b0b226d35ccc7989ad36784e
Kevin-Thesus <kevinthesus24@gmail.com>
2023-06-16 15:20:57 +0530

    first commit

---
+++ bitwise.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a = 44;
+int b = 24;
+
+//cout<<(a&b)<<endl;
+//cout<<(a|b)<<endl;
+//cout<<(~b)<<endl;
+
+
+if(a < b){
+    cout<< "a and b is not equal";
+}else if(a>b){
+    cout<< "a is greater than b";
+}
+else{
+    cout<< "a and b is equal";
+}
+
+}
